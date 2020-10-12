5a363d6b1f353110bba779036bdf3732c96e3a0b
Steve Meddaugh <steve.meddaugh@symetra.com>
2020-10-11 17:46:04 -0700

    update frontend apiHost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://1fe181f606226be366592a0a96218517.gr7.us-west-2.eks.amazonaws.com:8100/home'
 };
 
 /*
